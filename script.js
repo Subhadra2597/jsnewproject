509ad663817f7d318f7bf90bfa3fa33dc5776348
Subhadra2597 <subhadraanju@gmail.com>
2024-09-13 16:10:39 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,14 @@
+function oddoreven(num)
+{
+    if(num%2==0)
+    {
+        console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+}
+for(let i=0;i<=10;i++)
+{
+    oddoreven(i)
+}
